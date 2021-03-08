c8486a472ff9c3e8839e7c2a2736e7cfdd312a03
christiano <christiano.pulsar@gmail.com>
2021-03-08 03:21:54 +0000

    add react bootstrap

--- canopus/src/App.js
+++ canopus/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import RootNavigation from './components/RootNavigation';
 
 function App() {
   return (
     <div className="App">
+      <RootNavigation/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
